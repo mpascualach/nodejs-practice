330c6c67fd3e7719be4d76e3cf31f5085897bd9a
mpascualach <miguelpacheson@hotmail.es>
2023-08-21 21:13:59 +0200

    first

--- myApp.js
+++ myApp.js
@@ -1,39 +1,11 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+const PORT = process.env.PORT || 3030;
 
+console.log("Hello World");
 
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
